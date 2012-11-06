1fbe72ed7e1abadd0fc134fbdc5cde87e4c6e8d1
James O'Beirne <james.obeirne@gmail.com>
2012-11-05 22:22:12 -0500

    histogram rewrite

--- miser/views/histogram.py
+++ miser/views/histogram.py
@@ -2,66 +2,74 @@
 
 from ..miser import dictToSortedList
 
+
 class Histogram(object):
-  """Takes in an `incomeDict` and `expensesDict`, dicts keyed by names and
-  paired by amounts gained or spent, respectively over a period of time.
-  Produces a bar graph of either.
-  """
+    """Takes in an `incomeDict` and `expensesDict`, dicts keyed by names and
+    paired by amounts gained or spent, respectively over a period of time.
+    Produces a bar graph of either.
+    """
 
+    def __init__(self, miser, numBars=60):
+        """Print a histogram of expenses."""
 
-  def __init__(self, miser, fromdt, todt, numBars = 60):
-    """Print a histogram of expenses."""
+        def keysToString(indict):
+            """Return a new dict that has converted `indict`'s keys from
+            Transaction to string."""
+            newD = {}
 
-    def keysToString(indict):
-      """Return a new dict that has converted `indict`'s keys from
-      Transaction to string."""
-      newD = {}
-      for k, v in indict.iteritems():
-        newD[k.name] = v
-      return newD
+            for k, v in indict.iteritems():
+                newD[k.name] = v
 
-    self.income = dictToSortedList(keysToString(miser.income(fromdt, todt)))
-    self.expenses = dictToSortedList(keysToString(miser.expenses(fromdt, todt)))
-    self.numBars = numBars
+            return newD
 
-    print "Total expenses: %f" % (sum(miser.expenses(fromdt, todt).values()))
+        self.income = dictToSortedList(keysToString(
+            miser.latestJson['income']))
 
-    sumStr = "\nProfile of expenses:"
-    sumStr += self.expensesBar
-    sumStr += "\n\nProfile of income:"
-    sumStr += self.incomeBar
+        self.expenses = dictToSortedList(keysToString(
+            miser.latestJson['expenses']))
 
-    print sumStr
+        self.numBars = numBars
 
-  def _createTextProfile(self, indict):
-    """Create a bar-graph like representation of expenses and income."""
+        totalExp = sum(miser.latestJson['expenses'].values())
+        totalInc = sum(miser.latestJson['income'].values())
 
-    keys = map(lambda x: x[0], indict)
-    vals = map(lambda x: x[1], indict)
+        print ("Residual: %f" % (totalInc + totalExp))
 
-    outstrs = ["\n"]
-    propDict = {}
-    total = sum(vals)
-    maxLenKey = max([len(a) for a in keys])
-    maxLenVal = max([len(repr(a)) for a in vals])
+        sumStr = "\nProfile of expenses:"
+        sumStr += self.expensesBar
+        sumStr += "\n\nProfile of income:"
+        sumStr += self.incomeBar
 
-    for k, v in indict:
-      outstr = " "
-      outstr += k.ljust(maxLenKey + 1)
-      outstr += ("%.2f" % v).ljust(maxLenVal + 1)
-      outstr += "-" * int(self.numBars * (v / total))
-      outstrs.append(outstr)
+        print sumStr
 
-    return "\n".join(outstrs)
+    def _createTextProfile(self, indict):
+        """Create a bar-graph like representation of expenses and income."""
 
-  @property
-  def incomeBar(self):
-    """Return a string which is a bar-graph style profile of income."""
-    return self._createTextProfile(self.income)
+        keys = map(lambda x: x[0], indict)
+        vals = map(lambda x: x[1], indict)
 
-  @property
-  def expensesBar(self):
-    """Return a string which is a bar-graph style profile of expenses."""
-    return self._createTextProfile(self.expenses)
+        outstrs = ["\n"]
+        total = sum(vals)
+        maxLenKey = max([len(a) for a in keys])
+        maxLenVal = max([len(repr(a)) for a in vals])
+
+        for k, v in indict:
+            outstr = " "
+            outstr += k.ljust(maxLenKey + 1)
+            outstr += ("%.2f" % v).ljust(maxLenVal + 1)
+            outstr += "-" * int(self.numBars * (v / total))
+            outstrs.append(outstr)
+
+        return "\n".join(outstrs)
+
+    @property
+    def incomeBar(self):
+        """Return a string which is a bar-graph style profile of income."""
+        return self._createTextProfile(self.income)
+
+    @property
+    def expensesBar(self):
+        """Return a string which is a bar-graph style profile of expenses."""
+        return self._createTextProfile(self.expenses)
 
 
